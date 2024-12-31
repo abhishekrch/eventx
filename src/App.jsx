e945a9619a136e5c598d8e679e0a5d47ac8436d5
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-01-01 02:55:08 +0530

    Added Ethers.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-function App() {
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
-  return (
+function App() {
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+
+    const connectWallet = async () => {
+      try {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        setProvider(provider);
+
+        const signer = await provider.getSigner();
+        setSigner(signer);
+        
+        const accounts = await provider.listAccounts();
+        const accountAddresses = accounts.map((account) => account.address);
+        setAccounts(accountAddresses);
+               
+      } catch (e) {
+        console.error("Error connecting wallet", e)
+      }    
+    };
+
+    useEffect(() => {
+      connectWallet();
+    }, [])
+
+  return(
     <>
-      EventX
+    <p>
+    {accounts[0]}
+    </p>
     </>
-  )
+  ) 
 }
 
-export default App
+export default App;
